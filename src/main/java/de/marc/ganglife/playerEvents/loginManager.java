5b308190d405fe835c3ac0c407e4405624174329
0x-Ganglife <147924490+0x-Ganglife@users.noreply.github.com>
2023-11-20 09:43:02 +0000

    .

--- src/main/java/de/marc/ganglife/playerEvents/loginManager.java
+++ src/main/java/de/marc/ganglife/playerEvents/loginManager.java
@@ -20,6 +20,7 @@ public class loginManager implements Listener {
         setUnique.getUniqueID(player.getUniqueId()).thenAccept(unique -> {
            if(unique.isPresent()) {
                player.sendMessage(main.prefix + "Willkommen zurück.");
+               main.playSuccessSound(player);
 
                setPremium.getPremium(player.getUniqueId()).thenAccept(premium -> {
                     if(premium.get() == 1) {
