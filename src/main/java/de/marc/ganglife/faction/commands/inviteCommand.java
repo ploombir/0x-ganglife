0264e7707c00b7b09413fe53c0c731199965a3d5
0x-Ganglife <147924490+0x-Ganglife@users.noreply.github.com>
2023-12-17 17:27:17 +0000

    edit inviteCommand

--- src/main/java/de/marc/ganglife/faction/commands/inviteCommand.java
+++ src/main/java/de/marc/ganglife/faction/commands/inviteCommand.java
@@ -2,6 +2,7 @@ package de.marc.ganglife.faction.commands;
 
 import de.marc.ganglife.Main.main;
 import de.marc.ganglife.dataSetter.items;
+import de.marc.ganglife.dataSetter.setFaction;
 import de.marc.ganglife.faction.methods.factionPrefixes;
 import de.marc.ganglife.faction.methods.sendFactionMessage;
 import de.marc.ganglife.playerdatas.UPlayer;
@@ -24,104 +25,117 @@ import java.util.HashMap;
 public class inviteCommand implements CommandExecutor {
 
     public static HashMap<Player, String> invitedFaction = new HashMap<>();
+
+    setFaction setFaction = new setFaction(main.getPlugin().getDatabaseAsync().getDataSource());
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(sender instanceof Player player) {
             UPlayer uPlayer = UPlayer.getUPlayer(player.getUniqueId());
+            setFaction.getFactionMembers(uPlayer.getFaction()).thenAccept(members -> {
+                        Bukkit.getScheduler().runTask(main.getPlugin(), () -> {
 
-            boolean found = Arrays.stream(player.getInventory().getContents())
-                    .filter(item -> item != null && item.getType() != Material.AIR)
-                    .filter(item -> item.getItemMeta().getDisplayName().equalsIgnoreCase(items.PHONE.getItem().getItemMeta().getDisplayName()))
-                    .anyMatch(item -> item.getType() == items.PHONE.getItem().getType());
+                            boolean found = Arrays.stream(player.getInventory().getContents())
+                                    .filter(item -> item != null && item.getType() != Material.AIR)
+                                    .filter(item -> item.getItemMeta().getDisplayName().equalsIgnoreCase(items.PHONE.getItem().getItemMeta().getDisplayName()))
+                                    .anyMatch(item -> item.getType() == items.PHONE.getItem().getType());
 
-            if(!found) {
-                player.sendMessage(main.pre_error + "§cDu benötigst ein Handy, um Fraktionen zu verwalten.");
-                main.playErrorSound(player);
-                return true;
-            }
+                            if(!found) {
+                                player.sendMessage(main.pre_error + "§cDu benötigst ein Handy, um Fraktionen zu verwalten.");
+                                main.playErrorSound(player);
+                                return ;
+                            }
 
-            if(args.length != 1) {
-                player.sendMessage(main.pre_error + "§cVerwendung: /invite <Spieler>");
-                main.playErrorSound(player);
-                return true;
-            }
+                            if(args.length != 1) {
+                                player.sendMessage(main.pre_error + "§cVerwendung: /invite <Spieler>");
+                                main.playErrorSound(player);
+                                return ;
+                            }
 
-            if(uPlayer.getFaction().equals("Zivilist")) {
-                player.sendMessage(main.pre_error + "§cDu bist in keiner Fraktion.");
-                main.playErrorSound(player);
-                return true;
-            }
-            if(uPlayer.getFactionRank() < 5) {
-                player.sendMessage(main.noperms);
-                main.playErrorSound(player);
-                return true;
-            }
-            Player target = Bukkit.getPlayer(args[0]);
+                            if(uPlayer.getFaction().equals("Zivilist")) {
+                                player.sendMessage(main.pre_error + "§cDu bist in keiner Fraktion.");
+                                main.playErrorSound(player);
+                                return ;
+                            }
+                            if(uPlayer.getFactionRank() < 5) {
+                                player.sendMessage(main.noperms);
+                                main.playErrorSound(player);
+                                return ;
+                            }
+                            Player target = Bukkit.getPlayer(args[0]);
 
-            if(target == null) {
-                player.sendMessage(main.notonline);
-                main.playErrorSound(player);
-                return true;
-            }
-            if(player == target) {
-                player.sendMessage(main.pre_error + "§cDu kannst dich nicht selber in deine Fraktion einladen.");
-                main.playErrorSound(player);
-                return true;
-            }
-            UPlayer uTarget = UPlayer.getUPlayer(target.getUniqueId());
+                            if(target == null) {
+                                player.sendMessage(main.notonline);
+                                main.playErrorSound(player);
+                                return ;
+                            }
+                            if(player == target) {
+                                player.sendMessage(main.pre_error + "§cDu kannst dich nicht selber in deine Fraktion einladen.");
+                                main.playErrorSound(player);
+                                return ;
+                            }
+                            UPlayer uTarget = UPlayer.getUPlayer(target.getUniqueId());
 
-            if(!uTarget.getFaction().equals("Zivilist")) {
-                player.sendMessage(main.pre_error + "§cDieser Spieler ist bereits in einer Fraktion.");
-                main.playErrorSound(player);
-                return true;
-            }
+                            if(!uTarget.getFaction().equals("Zivilist")) {
+                                player.sendMessage(main.pre_error + "§cDieser Spieler ist bereits in einer Fraktion.");
+                                main.playErrorSound(player);
+                                return ;
+                            }
 
-            if(player.getLocation().distance(target.getLocation()) >= 3) {
-                player.sendMessage(main.pre_error + "§cDieser Spieler ist nicht in deiner nähe.");
-                main.playErrorSound(player);
-                return true;
-            }
+                            if(player.getLocation().distance(target.getLocation()) >= 3) {
+                                player.sendMessage(main.pre_error + "§cDieser Spieler ist nicht in deiner nähe.");
+                                main.playErrorSound(player);
+                                return ;
+                            }
 
-            invitedFaction.put(target, uPlayer.getFaction());
+                            if(members.size() > 10) {
+                                player.sendMessage(main.pre_error + "§cEs sind bereits 10 Mitglieder in deiner Fraktion.");
+                                main.playErrorSound(player);
+                                return ;
+                            }
 
-            Gui inviteInventory = Gui.gui()
-                    .rows(3)
-                    .title(Component.text(main.prefix + factionPrefixes.getPrefix(player) + invitedFaction.get(target)))
-                    .disableAllInteractions()
-                    .create();
+                            invitedFaction.put(target, uPlayer.getFaction());
 
-            player.sendMessage(main.prefix + "§7Du hast §6" + target.getName() + " §7in die Fraktion " + factionPrefixes.getPrefix(player)
-                    + uPlayer.getFaction() + " §7eingeladen.");
-            main.playProccessSound(player);
-            main.playProccessSound(target);
+                            Gui inviteInventory = Gui.gui()
+                                    .rows(3)
+                                    .title(Component.text(main.prefix + factionPrefixes.getPrefix(player) + invitedFaction.get(target)))
+                                    .disableAllInteractions()
+                                    .create();
 
-            GuiItem inviteGUI = ItemBuilder.from(Material.GUNPOWDER)
-                    .name(Component.text("§aFraktion beitreten.."))
-                    .lore(Component.text(" §f▹ §7§oKlicke um die Einladung anzunehmen."))
-                    .asGuiItem(clickEvent -> {
-                        uTarget.setFaction(invitedFaction.get(target));
-                        uTarget.setFactionRank(1);
-                        target.sendMessage(main.prefix + "§7Du bist der Fraktion " + factionPrefixes.getPrefix(player) + invitedFaction.get(target) + " §7beigetreten.");
-                        sendFactionMessage.sendPlayerFactionMessage(player, target.getName() + " ist der Fraktion beigetreten.");
-                        main.playSuccessSound(player);
-                        main.playSuccessSound(target);
-                        target.closeInventory();
+                            player.sendMessage(main.prefix + "§7Du hast §6" + target.getName() + " §7in die Fraktion " + factionPrefixes.getPrefix(player)
+                                    + uPlayer.getFaction() + " §7eingeladen.");
+                            main.playProccessSound(player);
+                            main.playProccessSound(target);
+
+                            GuiItem inviteGUI = ItemBuilder.from(Material.GUNPOWDER)
+                                    .name(Component.text("§aFraktion beitreten.."))
+                                    .lore(Component.text(" §f▹ §7§oKlicke um die Einladung anzunehmen."))
+                                    .asGuiItem(clickEvent -> {
+                                        uTarget.setFaction(invitedFaction.get(target));
+                                        uTarget.setFactionRank(1);
+                                        target.sendMessage(main.prefix + "§7Du bist der Fraktion " + factionPrefixes.getPrefix(player) + invitedFaction.get(target) + " §7beigetreten.");
+                                        sendFactionMessage.sendPlayerFactionMessage(player, target.getName() + " ist der Fraktion beigetreten.");
+                                        main.playSuccessSound(player);
+                                        main.playSuccessSound(target);
+                                        target.closeInventory();
+                                    });
+
+                            inviteInventory.setCloseGuiAction(close -> {
+                                if(!uTarget.getFaction().equals("Zivilist")) {
+                                    return;
+                                }
+                                player.sendMessage(main.prefix + "§6" + target.getName() + " §7hat die Einladung §cabgelehnt.");
+                                target.sendMessage(main.prefix + "§7Du hast die Einladung §cabgelehnt.");
+                                main.playErrorSound(player);
+                                main.playErrorSound(target);
+                                invitedFaction.remove(target);
+                            });
+
+                            inviteInventory.setItem(13, inviteGUI);
+
+                            inviteInventory.open(target);
+                        });
                     });
-
-            inviteInventory.setCloseGuiAction(close -> {
-                if(!uTarget.getFaction().equals("Zivilist")) {
-                    return;
-                }
-                player.sendMessage(main.prefix + "§6" + target.getName() + " §7hat die Einladung §cabgelehnt.");
-                target.sendMessage(main.prefix + "§7Du hast die Einladung §cabgelehnt.");
-                main.playErrorSound(player);
-                main.playErrorSound(target);
-                invitedFaction.remove(target);
-            });
-
-            inviteInventory.setItem(13, inviteGUI);
-
-            inviteInventory.open(target);
+            
          }
         return false;
     }
